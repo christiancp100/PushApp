b8d1b198560eced27d9275809e34d308d083b6db
erickgarro <erickgarro@gmail.com>
2019-11-23 19:50:44 +0100

    Updated required libraries and added Socket.io emitter server

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const dust = require('klei-dust');
 const methodOverride = require('method-override');
 const routers = require('./routes/routers');
+const bodyParser = require('body-parser');
 const EventEmitter = require('events');
 const mongoose = require('mongoose');
+const express = require('express');
+const dust = require('klei-dust');
+const logger = require('morgan');
+const path = require('path');
 const app = express();
 
 // Configure app
@@ -26,14 +26,6 @@ app.use('/', routers.root);
 app.use('/test/fetch', routers.fetch_tests);
 app.use('/users', routers.users);
 
-// Catch 404 and forward to error handler
-// This should be configured after all 200 routes
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 // Mongoose connection to MongoDB and Collection name declaration
 mongoose.connect('mongodb://localhost/PushApp');
 
@@ -44,6 +36,14 @@ const emitter = new EventEmitter()
 app.set('view engine', 'dust');
 app.set('views', __dirname + '/views');
 app.engine('dust', dust.dust);
+
+// Catch 404 and forward to error handler
+// This should be configured after all 200 routes
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 module.exports = app;
 module.exports = emitter;
