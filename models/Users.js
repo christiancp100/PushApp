0fa3aba9d836237c77a43b9f531d6338684c81b1
Maxim Ryakhovskiy <ryakhm@usi.ch>
2019-11-25 09:30:39 +0100

    Access model is created for username and password, and the user-register.dust

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Access = mongoose.model('Access');
 
 const UsersSchema = exports.UsersSchema = new Schema({
+  access : Access,
   firstName: {type: String, required: true},
   lastName: {type: String, required: true},
   description: {type: String},
