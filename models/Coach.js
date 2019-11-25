793c00cad68c056435b59a0669de772d1771a4b6
CarattiJ <jacopo.caratti@bluewin.ch>
2019-11-25 22:47:37 +0100

    coach/router.js
    setResponse
    put
    getFilter
    post

--- models/Coach.js
+++ models/Coach.js
@@ -7,9 +7,9 @@ const Service = require('./Service');
 
 const CoachSchema = exports.CoachSchema = new Schema({
     userAccount: {type: UserAccount, required: true},
-    certificate: {type: [Certificate], default: () => []},
-    rating: {type: [Rating], default: () => []},
-    service: {type: [Service], default: () => []},
+    certificates: {type: [Certificate], default: () => []},
+    ratings: {type: [Rating], default: () => []},
+    services: {type: [Service], default: () => []},
 });
 
 mongoose.model('Coach', CoachSchema);
--- routes/coach/router.js
+++ routes/coach/router.js
@@ -15,7 +15,7 @@ function getCoaches(req, callback) {
                     console.log('Coaches collection retrieved from database.');
                     callback(found);
                 } else {
-                    console.log('Empty coach collection retrieved from database.');
+                    console.log('Empty coaches collection retrieved from database.');
                     callback(found);
                 }
             } else {
@@ -23,7 +23,7 @@ function getCoaches(req, callback) {
             }
         })
         .catch((err) => {
-            console.log("Error:  " + err.message);
+            console.log("Error: " + err.message);
         });
 }
 
@@ -31,24 +31,33 @@ function getCoaches(req, callback) {
 router.post('/', function (req, res) {
     if ((req.get('Content-Type') === "application/json" && req.accepts("application/json")) || req.get('Content-Type') === "application/x-www-form-urlencoded" && req.body !== undefined) {
         console.log('Creating new coach...');
-        if ('firstName' in req.body === undefined && 'lastName' in req.body === undefined && 'birthday' in req.body === undefined && 'sex' in req.body === undefined) {
-            res = setResponse('json', 400, res, {Error: "First name, last name, birthday, sex, and phone number must be provided"});
+        if ('firstName' in req.body.userAccount === undefined || 'lastName' in req.body.userAccount === undefined || 'birthday' in req.body.userAccount === undefined ||
+            'sex' in req.body.userAccount === undefined || 'email' in req.body.userAccount === undefined || 'phone' in req.body.userAccount === undefined ||
+            'address1' in req.body.userAccount === undefined || 'city' in req.body.userAccount === undefined || 'state' in req.body.userAccount === undefined ||
+            'zipCode' in req.body.userAccount === undefined || 'country' in req.body.userAccount === undefined) {
+            res = setResponse('json', 400, res, {Error: "First name, last name, birthday, sex, email address, phone number, at least one address, city, state, zip code and country must be provided"});
             res.end();
         } else {
             const coach = new Coach({
-                // firstName: req.body.firstName,
-                // lastName: req.body.lastName,
-                // description: req.body.description,
-                // photo: req.body.photo,
-                // birthday: req.body.birthday,
-                // sex: req.body.sex,
-                // email: req.body.email,
-                // phone: req.body.phone,
-                // certificate: [Certificate],
-                // city: req.body.city,
-                // state: req.body.state,
-                // zipCode: req.body.zipCode,
-                // country: req.body.country,
+                userAccount: {
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    description: req.body.description,
+                    photo: req.body.photo,
+                    birthday: req.body.birthday,
+                    sex: req.body.sex,
+                    email: req.body.email,
+                    phone: req.body.phone,
+                    address1: req.body.address1,
+                    address2: req.body.address2,
+                    city: req.body.city,
+                    state: req.body.state,
+                    country: req.body.country,
+                    currency: req.body.currency,
+                    localization: req.body.localization,
+                },
+                certificates: req.body.certificates,
+                services: req.body.services,
             });
 
             coach.save().then((saved) => {
@@ -70,39 +79,77 @@ router.post('/', function (req, res) {
     }
 });
 
-// Create search filter
+// Creates filter for searching coaches on the database
 function getFilter(req) {
     const filter = {};
-    // Search by coach ID
-    if (req.query._id != undefined && !mongoose.Types.ObjectId.isValid(req.query._id && req.query.firstName === undefined && req.query.lastName === undefined)) {
-        filter._id = req.query._id;
-        // Search by coach ID, first name and last name
-    } else if (req.query._id !== undefined && req.query.firstName !== undefined && req.query.lastName !== undefined) {
-        filter._id = req.query._id;
-        filter.name = req.query.firstName;
-        filter.name = req.query.lastName;
-        // Search by coach ID and last name
-    } else if (req.query._id !== undefined && req.query.firstName === undefined && req.query.lastName !== undefined) {
-        filter._id = req.query._id;
-        filter.name = req.query.lastName;
-    } else {
-        console.log('You need a valid coach ID!');
-        throw Error('You need a valid coach ID!');
+    let request;
+
+    if (Object.keys(req.body).length > 0) {
+        request = req.body;
+    } else if (Object.keys(req.query).length > 0) {
+        request = req.query;
     }
-    // Search by coach sex
-    if (req.params.sex !== undefined || req.query.sex !== undefined) {
-        filter.sex = req.params.sex;
-    } else if (req.query.sex !== undefined) {
-        filter.sex = req.query.sex;
+
+    if (request !== undefined) {
+        //Filter based on:
+        // ID
+        if (request.id !== undefined && mongoose.Types.ObjectId.isValid(request.id)) {
+            filter._id = request.id;
+        }
+
+        // First name
+        if (request.firstName !== undefined) {
+            filter.firstName = request.firstName;
+        }
+
+        // Last name
+        if (request.lastName !== undefined) {
+            filter.lastName = request.lastName;
+        }
+
+        // Birthday
+        if (request.birthday !== undefined) {
+            filter.birthday = request.birthday;
+        }
+
+        // Sex
+        if (request.sex !== undefined) {
+            filter.sex = request.sex;
+        }
+
+        // City
+        if (request.city !== undefined) {
+            filter.city = request.city;
+        }
+
+        // State
+        if (request.state !== undefined) {
+            filter.state = request.state;
+        }
+
+        // Country
+        if (request.country !== undefined) {
+            filter.country = request.country;
+        }
+
+        // Certificates
+        if (request.certificates !== undefined) {
+            filter.certificates = request.certificates;
+        }
+
+        // Services
+        if (request.services !== undefined) {
+            filter.services = request.services;
+        }
+
+        // Search non deleted
+        if (request.isDeleted === undefined) {
+            filter.isDeleted = false;
+        } else {
+            filter.isDeleted = request.isDeleted;
+        }
+        return filter;
     }
-    // Search by coach city
-    if (req.params.city !== undefined || req.query.city !== undefined) {
-        filter.city = req.params.city;
-    } else if (req.query.city !== undefined) {
-        filter.city = req.query.city;
-    }
-    //to discuss - search by city, ...?
-    return filter;
 }
 
 // Search for coach
@@ -110,8 +157,113 @@ router.get('/search', function (req, res) {
 
 });
 
-function setResponse(type, code, res, msg) {
+// Edit a coach
+router.put('/edit/:id', function (req, res) {
+    if (req.accepts("json")) {
+        if (req.params.id !== undefined && !mongoose.Types.ObjectId.isValid(req.params.id)) {
+            res.status(400).end();
+        } else {
+            console.log('Searching for coach with ID: ' + req.params.id + '.');
+            Coach.findById({_id: req.params.id})
+                .then((found) => {
+                        if (found != null) {
+                            found.userAccount.firstName = req.body.firstName;
+                            found.userAccount.lastName = req.body.lastName;
+                            found.userAccount.description = req.body.description;
+                            found.userAccount.photo = req.body.photo;
+                            found.userAccount.birthday = req.body.birthday;
+                            found.userAccount.sex = req.body.sex;
+                            found.userAccount.email = req.body.email;
+                            found.userAccount.phone = req.body.phone;
+                            found.userAccount.address1 = req.body.address1;
+                            found.userAccount.address2 = req.body.address2;
+                            found.userAccount.city = req.body.city;
+                            found.userAccount.state = req.body.state;
+                            found.userAccount.country = req.body.country;
+                            found.userAccount.currency = req.body.currency;
+                            found.userAccount.localization = req.body.localization;
+                            found.userAccount.certificates = req.body.certificates;
+                            found.userAccount.services = req.body.services;
+                            return found.save()
+                        }
+                    },
+                    (err) => {
+                        res = setResponse('error', 404, res, {Error: 'Favorite not found!'});
+                    })
+                .then((saved) => {
+                    console.log('Coach with ID: ' + req.params.id + ' updated!');
+                    if (req.accepts("text/html")) {
+                        res = setResponse('html', 201, res);
+                        res.redirect('/');
+                    } else if (req.accepts("application/json")) {
+                        res = setResponse('json', 201, res, saved);
+                        res.end();
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500);
+                    res.end();
+                });
+        }
+    }
+});
 
+// Soft delete a coach
+router.delete('/delete/:id', function (req, res) {
+    if (req.accepts("json")) {
+        if (req.params.id !== undefined && !mongoose.Types.ObjectId.isValid(req.params.id)) {
+            res.status(400).end();
+        } else {
+            console.log('Searching for user with ID: ' + req.params.id + '.');
+            Coach.findById({_id: req.params.id})
+                .then((found) => {
+                        if (found != null) {
+                            found.isDeleted = true;
+                            return found.save()
+                        }
+                    },
+                    (err) => {
+                        res = setResponse('error', 404, res, {Error: 'Coach not found!'});
+                    })
+                .then((saved) => {
+                    console.log('Coach with ID: ' + req.params.id + ' was soft deleted!');
+                    if (req.accepts("text/html")) {
+                        res = setResponse('html', 201, res);
+                        res.redirect('/');
+                    } else if (req.accepts("application/json")) {
+                        res = setResponse('json', 201, res, saved);
+                        res.end();
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500);
+                    res.end();
+                });
+        }
+    }
+});
+
+// Customized response
+function setResponse(type, code, res, msg) {
+    res.status(code);
+    switch (type) {
+        case 'json':
+            res.set('Content-Type', 'application/json');
+            res.json(msg);
+            return res;
+            break;
+        case 'html':
+            return res.set('Content-Type', 'text/html');
+            break;
+        case 'error':
+            res.json(msg);
+            return res;
+            break;
+        default:
+            break;
+    }
 }
 
 module.exports = router;
