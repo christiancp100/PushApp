24bd3c4d9ad0270830d74ffe09570f064f5465de
erickgarro <erickgarro@gmail.com>
2019-11-27 12:36:38 +0100

    Update Credential.js

--- models/Credential.js
+++ models/Credential.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CredentialsSchema = exports.CredentialsSchema = new Schema({
-    _clientId: {type: Schema.Types.ObjectId, ref: 'Client', required: true},
     username: {type: String, required: true},
     password: {type: String, required: true}
 });
