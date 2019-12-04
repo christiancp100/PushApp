c723840a2e4611995e96f33f76fcf03edc2fbec7
davidetrupia <trupid@usi.ch>
2019-12-04 19:28:48 +0100

    all coaches are now displayed in client/coaches and front-end for search
    Listing all coaches both front-end and back-end done;
    Search needs the backend and is done

--- public/js/session.js
+++ public/js/session.js
@@ -1,41 +1,92 @@
-async function getExercises() {
-    try {
-        let headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        };
 
-        let userId = await fetch("/auth/getuserinfo");
-        userId = await userId.json();
+getUserId = async() => {
+    let obj = await fetch('/auth/getuserinfo');
+    obj = await obj.json();
+    return obj.userAccountId;
+};
+// let A = [];
+async function getCoaches(){
 
-        let session = await fetch("/workouts/sessions/search?_clientId=" + userId.userAccountId + "&weekday=" + getWeekDay(), {
-            method: 'GET',
-            headers: headers,
-        });
+    let everyone = await fetch("/coaches/search?accountType=coach");
+    let coachesArray = await everyone.json();
+    console.log("all coaches: ", coachesArray);
+    for(let i = 0; i<coachesArray.length; i++){
 
-        session = await session.json();
+        let div = document.getElementById("grid");
 
-        let exercises = [];
-        for (let i = 0; i < session.exercises.length; i++) {
-            let exercise = await fetch("/workouts/exercises/search?id=" + session.exercises[i], {
-                method: 'GET',
-                headers: headers
-            });
-            exercise = await exercise.json();
-            exercises.push(exercise);
-        }
+        let card = document.createElement("div");
+        card.className = "card col-lg-4 col-xs-12";
+        div.appendChild(card);
 
-        dust.render("dashboard_partials\/schedule_table_row",
-            {exercises: exercises}, (err, out) =>
-                document.getElementById('scheduleTable').innerHTML = out);
-    } catch (err) {
-        console.log(err);
+        let img = document.createElement("img");
+        img.src= coachesArray[i].photo;
+        card.appendChild(img);
+
+        let h2 = document.createElement("h2");
+        h2.innerHTML = coachesArray[i].firstName +" "+ coachesArray[i].lastName;
+        img.insertAdjacentElement("afterend", h2);
+
+        let p_description = document.createElement("p");
+        p_description.innerHTML = coachesArray[i].description;
+        h2.insertAdjacentElement("afterend", p_description);
+
+        let p = document.createElement("p");
+        p.innerHTML = coachesArray[i].birthday + coachesArray[i].state + coachesArray[i].city;
+        p_description.insertAdjacentElement("afterend", p);
+
     }
 }
 
-function getWeekDay() {
-    const weekdays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-    return weekdays[new Date().getDay()];
-}
 
-getExercises();
+
+
+
+
+
+
+
+
+//
+// async function getExercises() {
+//     try {
+//         let headers = {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         };
+//
+//         let userId = await fetch("/auth/getuserinfo");
+//         userId = await userId.json();
+//
+//         let session = await fetch("/workouts/sessions/search?_clientId=" + userId.userAccountId + "&weekday=" + getWeekDay(), {
+//             method: 'GET',
+//             headers: headers,
+//         });
+//
+//         session = await session.json();
+//
+//         let exercises = [];
+//         for (let i = 0; i < session.exercises.length; i++) {
+//             let exercise = await fetch("/workouts/exercises/search?id=" + session.exercises[i], {
+//                 method: 'GET',
+//                 headers: headers
+//             });
+//             exercise = await exercise.json();
+//             exercises.push(exercise);
+//         }
+//
+//         dust.render("dashboard_partials\/schedule_table_row",
+//             {exercises: exercises}, (err, out) =>
+//                 document.getElementById('scheduleTable').innerHTML = out);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+//
+// function getWeekDay() {
+//     const weekdays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+//     return weekdays[new Date().getDay()];
+// }
+//
+// getExercises();
+
+
