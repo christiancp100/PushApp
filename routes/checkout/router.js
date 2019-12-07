dbfbe76892679571079c3072979b5139a065c5e1
erickgarro <erickgarro@gmail.com>
2019-12-07 12:34:36 +0100

    Payments are being processed by Stripe (integration with PrushApp workflow starts now)

---
+++ routes/checkout/router.js
@@ -0,0 +1,85 @@
+/** @module root/router */
+'use strict';
+
+require("dotenv").config({path: "../.env"});
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+router.use(
+    express.json({
+        // We need the raw body to verify webhook signatures.
+        // Let's compute it only when hitting the Stripe webhook endpoint.
+        verify: function (req, res, buf) {
+            if (req.originalUrl.startsWith("/webhook")) {
+                req.rawBody = buf.toString();
+            }
+        }
+    })
+);
+
+const calculateOrderAmount = items => {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the client to prevent
+    // people from directly manipulating the amount on the server
+    return 14000;
+};
+
+router.post("/create-payment-intent", async (req, res) => {
+    const {items, currency} = req.body;
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: currency
+    });
+
+    // Send publishable key and PaymentIntent details to server
+    res.send({
+        publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+        clientSecret: paymentIntent.client_secret
+    });
+});
+
+// Expose a endpoint as a webhook handler for asynchronous events.
+// Configure your webhook in the stripe developer dashboard
+// https://dashboard.stripe.com/test/webhooks
+router.post("/webhook", async (req, res) => {
+    let data, eventType;
+
+    // Check if webhook signing is configured.
+    if (process.env.STRIPE_WEBHOOK_SECRET) {
+        // Retrieve the event by verifying the signature using the raw body and secret.
+        let event;
+        let signature = req.headers["stripe-signature"];
+        try {
+            event = stripe.webhooks.constructEvent(
+                req.rawBody,
+                signature,
+                process.env.STRIPE_WEBHOOK_SECRET
+            );
+        } catch (err) {
+            console.log(`⚠️  Webhook signature verification failed.`);
+            return res.sendStatus(400);
+        }
+        data = event.data;
+        eventType = event.type;
+    } else {
+        // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+        // we can retrieve the event data directly from the request body.
+        data = req.body.data;
+        eventType = req.body.type;
+    }
+
+    if (eventType === "payment_intent.succeeded") {
+        // Funds have been captured
+        // Fulfill any orders, e-mail receipts, etc
+        // To cancel the checkout after capture you will need to issue a Refund (https://stripe.com/docs/api/refunds)
+        console.log("💰 Payment captured!");
+    } else if (eventType === "payment_intent.payment_failed") {
+        console.log("❌ Payment failed.");
+    }
+    res.sendStatus(200);
+});
+
+module.exports = router;
--- routes/root/router.js
+++ routes/root/router.js
@@ -21,27 +21,35 @@ let ClientInfo = mongoose.model('ClientInfo');
 let CoachClients = mongoose.model('CoachClients');
 
 router.get('/test', function (req, res) {
-    res.render('rating/rating-first.dust', { name: 'Moreno', id: '5de5094ec516ae82b90c9c44' });
-})
+    res.render('rating/rating-first.dust', {name: 'Moreno', id: '5de5094ec516ae82b90c9c44'});
+});
+
 router.get('/testing', function (req, res) {
-    res.render('rating/rating-again.dust', { name: 'Moreno', score: 4, comment: "HE was very good", title: "awesome", objId: '5de7f4e3d9511123b9bfd669' });
-})
+    res.render('rating/rating-again.dust', {
+        name: 'Moreno',
+        score: 4,
+        comment: "HE was very good",
+        title: "awesome",
+        objId: '5de7f4e3d9511123b9bfd669'
+    });
+});
+
 router.get('/', function (req, res, next) {
     if (req.accepts("html")) {
         if (typeof req.user !== "undefined" && req.isAuthenticated()) {
             console.log("ENTER");
-            res.render('index', { title: 'PushApp', log: 'Y' });
+            res.render('index', {title: 'PushApp', log: 'Y'});
         } else {
             console.log("NOT ENTER");
-            res.render('index', { title: 'PushApp', log: 'N' });
+            res.render('index', {title: 'PushApp', log: 'N'});
         }
     } else {
         // res.status(500);
         res.end();
     }
 });
-/*render index page without signup button and login Buttons*/
 
+// Renders index page without signup button and login buttons
 function isLoggedIn(req, res, next) {
     console.log(req.path);
     if (req.user === undefined) {
@@ -65,13 +73,18 @@ function isLoggedIn(req, res, next) {
   }
 })*/
 
+// Payments testing page
+router.get('/checkout', async (req, res) => {
+    console.log('Rendering checkout html...');
+    res.render('checkout');
+    res.end();
+});
 
 // Dynamic user route according to userAccount type
 router.get('/:username', isLoggedIn, async (req, res, next) => {
     try {
         if (req.accepts("html")) {
             const filter = getFilter(req);
-
 
             if (req.path === '/login') {
                 res.render('login.dust')
@@ -81,19 +94,16 @@ router.get('/:username', isLoggedIn, async (req, res, next) => {
                 res.render('register_forms/coach-register');
             } else if (req.path === '/register-client') {
                 res.render('register_forms/client-register');
-            } else if (req.path === '/payments') { // Stripe
-                res.render('payment_checkout');
             } else if (req.path === '/workout') {
                 res.render('workout');
-            }
-            else {
+            } else {
                 let credentials = await Credentials.findOne(filter);
                 console.log();
                 if (credentials === null || credentials.username !== filter.username) {
                     // CHANGE FOR CORRECT 404 PAGE
-                    res = setResponse('json', 401, res, { Error: 'Unauthorized access!' });
+                    res = setResponse('json', 401, res, {Error: 'Unauthorized access!'});
                 } else {
-                    let activeUser = await UserAccount.findById({ _id: credentials._userAccountId });
+                    let activeUser = await UserAccount.findById({_id: credentials._userAccountId});
 
                     if (activeUser.accountType === 'client') {
                         await renderClientDashboard(res, activeUser);
@@ -110,12 +120,11 @@ router.get('/:username', isLoggedIn, async (req, res, next) => {
         }
         res.end();
     } catch
-    (err) {
+        (err) {
         res.status(500);
         res.end();
     }
-})
-    ;
+});
 
 async function renderClientDashboard(res, activeUser) {
     console.log("PHOTO", activeUser.photo);
@@ -124,34 +133,34 @@ async function renderClientDashboard(res, activeUser) {
     }
     let menu = {
         user:
-        {
-            firstName: activeUser.firstName,
-            photo: activeUser.photo
-        }
+            {
+                firstName: activeUser.firstName,
+                photo: activeUser.photo
+            }
         ,
         items: [
-            { name: "Dashboard", icon: "web" },
-            { name: "Next Workout", icon: "list" },
-            { name: "Schedule", icon: "dashboard" },
-            { name: "Chat", icon: "chat" },
-            { name: "Coaches", icon: "group" },
+            {name: "Dashboard", icon: "web"},
+            {name: "Next Workout", icon: "list"},
+            {name: "Schedule", icon: "dashboard"},
+            {name: "Chat", icon: "chat"},
+            {name: "Coaches", icon: "group"},
         ],
         accordions: [
             {
                 title: "Progress",
                 icon: "chevron_left",
                 subItems: [
-                    { name: "Weight", icon: "show_chart" },
-                    { name: "Exercises", icon: "equalizer" },
-                    { name: "Volume of Training", icon: "multiline_chart" },
+                    {name: "Weight", icon: "show_chart"},
+                    {name: "Exercises", icon: "equalizer"},
+                    {name: "Volume of Training", icon: "multiline_chart"},
                 ]
             },
             {
                 title: "Account",
                 icon: "chevron_left",
                 subItems: [
-                    { name: "Logout", icon: "person", logout: true },
-                    { name: "Settings", icon: "settings", accountType: 'clients' },
+                    {name: "Logout", icon: "person", logout: true},
+                    {name: "Settings", icon: "settings", accountType: 'clients'},
                 ]
             }
         ]
@@ -165,7 +174,7 @@ async function clientsDropdown(activeUser) {
         return [];
     }
     try {
-        let result = await CoachClients.find({ _coachId: activeUser.id });
+        let result = await CoachClients.find({_coachId: activeUser.id});
         if (result) {
             console.log(result);
             if (result.length > 0) {
@@ -203,31 +212,31 @@ async function renderCoachDashboard(res, activeUser) {
     }
     let menu = {
         user: [
-            { firstName: "Coach " + activeUser.firstName },
-            { photo: activeUser.photo }
+            {firstName: "Coach " + activeUser.firstName},
+            {photo: activeUser.photo}
         ],
         items: [
-            { name: "Dashboard", icon: "web" },
-            { name: "Clients", icon: "list" },
-            { name: "Schedules", icon: "dashboard" },
-            { name: "Chat", icon: "chat" },
+            {name: "Dashboard", icon: "web"},
+            {name: "Clients", icon: "list"},
+            {name: "Schedules", icon: "dashboard"},
+            {name: "Chat", icon: "chat"},
         ],
         accordions: [
             {
                 title: "Accounting",
                 icon: "chevron_left",
                 subItems: [
-                    { name: "Revenue", icon: "show_chart" },
-                    { name: "Users", icon: "equalizer" },
-                    { name: "Conversion Rate", icon: "multiline_chart" },
+                    {name: "Revenue", icon: "show_chart"},
+                    {name: "Users", icon: "equalizer"},
+                    {name: "Conversion Rate", icon: "multiline_chart"},
                 ]
             },
             {
                 title: "Account",
                 icon: "chevron_left",
                 subItems: [
-                    { name: "Logout", icon: "person", logout: "true" },
-                    { name: "Settings", icon: "settings", accountType: "coaches" },
+                    {name: "Logout", icon: "person", logout: "true"},
+                    {name: "Settings", icon: "settings", accountType: "coaches"},
                 ]
             }
         ],
@@ -326,28 +335,28 @@ function setResponse(type, code, res, msg) {
 router.get('/coach/dashboard/clients', (req, res) => {
     let menu = {
         items: [
-            { name: "Dashboard", icon: "web" },
-            { name: "Clients", icon: "list" },
-            { name: "Schedules", icon: "dashboard" },
-            { name: "Chat", icon: "chat" },
-            { link: "/client/coaches", name: "Coaches", icon: "group", },
+            {name: "Dashboard", icon: "web"},
+            {name: "Clients", icon: "list"},
+            {name: "Schedules", icon: "dashboard"},
+            {name: "Chat", icon: "chat"},
+            {link: "/client/coaches", name: "Coaches", icon: "group",},
         ],
         accordions: [
             {
                 title: "Accounting",
                 icon: "chevron_left",
                 subItems: [
-                    { name: "Revenue", icon: "show_chart" },
-                    { name: "Users", icon: "equalizer" },
-                    { name: "Conversion Rate", icon: "multiline_chart" },
+                    {name: "Revenue", icon: "show_chart"},
+                    {name: "Users", icon: "equalizer"},
+                    {name: "Conversion Rate", icon: "multiline_chart"},
                 ]
             },
             {
                 title: "Account",
                 icon: "chevron_left",
                 subItems: [
-                    { name: "Logout", icon: "person" },
-                    { name: "Settings", icon: "settings" },
+                    {name: "Logout", icon: "person"},
+                    {name: "Settings", icon: "settings"},
                 ]
             }
         ]
@@ -358,28 +367,28 @@ router.get('/coach/dashboard/clients', (req, res) => {
 router.get('/client/coaches', (req, res) => {
     let menu = {
         items: [
-            { name: "Dashboard", icon: "web" },
-            { name: "Next Workout", icon: "list" },
-            { name: "Schedule", icon: "dashboard" },
-            { name: "Chat", icon: "chat" },
-            { name: "Coaches", icon: "group" },
+            {name: "Dashboard", icon: "web"},
+            {name: "Next Workout", icon: "list"},
+            {name: "Schedule", icon: "dashboard"},
+            {name: "Chat", icon: "chat"},
+            {name: "Coaches", icon: "group"},
         ],
         accordions: [
             {
                 title: "Progress",
                 icon: "chevron_left",
                 subItems: [
-                    { name: "Weight", icon: "show_chart" },
-                    { name: "Exercises", icon: "equalizer" },
-                    { name: "Volume of Training", icon: "multiline_chart" },
+                    {name: "Weight", icon: "show_chart"},
+                    {name: "Exercises", icon: "equalizer"},
+                    {name: "Volume of Training", icon: "multiline_chart"},
                 ]
             },
             {
                 title: "Account",
                 icon: "chevron_left",
                 subItems: [
-                    { name: "Logout", icon: "person", logout: true },
-                    { name: "Settings", icon: "settings" },
+                    {name: "Logout", icon: "person", logout: true},
+                    {name: "Settings", icon: "settings"},
                 ]
             }
         ]
