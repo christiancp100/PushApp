31f081e9eda181924bd69798d6afbad0777063fc
erickgarro <erickgarro@gmail.com>
2019-12-07 10:21:07 +0100

    reactivated getExercises()

--- public/js/session.js
+++ public/js/session.js
@@ -1,63 +1,68 @@
-
-getUserId = async() => {
+getUserId = async () => {
     let obj = await fetch('/auth/getuserinfo');
     obj = await obj.json();
     return obj.userAccountId;
 };
+
 // let A = [];
-function prettyPrinterDate(src){
-    let partial ='';
-    for(let i=0; i<src.length; i++){
+function prettyPrinterDate(src) {
+    let partial = '';
+    for (let i = 0; i < src.length; i++) {
         let x = src.charAt(i);
-        if(x === 'T'){
+        if (x === 'T') {
             return partial;
-        }else if(x === '-'){
+        } else if (x === '-') {
             x = '/';
-            partial+=x;
-        }else{
-            partial +=x;
+            partial += x;
+        } else {
+            partial += x;
         }
     }
 }
-searchCoaches = async()=>{
+
+searchCoaches = async () => {
     let txt = document.getElementById("last_name").value;
-    if(txt === ''|| txt === " "){
+    if (txt === '' || txt === " ") {
         getCoaches();
     }
-    let found = await fetch("/coaches/search?accountType=coach&firstName="+txt);
+    let found = await fetch("/coaches/search?accountType=coach&firstName=" + txt);
     let foundArray = await found.json();
     cleanCards();
     await displayCoaches(foundArray);
 };
 
-cleanCards = ()=>{
+cleanCards = () => {
     let children = document.getElementById("grid").childNodes;
-    for(let i =0;i<children.length;i++) {
+    for (let i = 0; i < children.length; i++) {
         children[i].remove();
     }
 
 };
-checkIfHiredAlready = async(id)=>{
-    let getting = await fetch("/coaches/hire/coach/"+id, {method:"GET", headers: {'Content-Type':'application/json'}});
+checkIfHiredAlready = async (id) => {
+    let getting = await fetch("/coaches/hire/coach/" + id, {
+        method: "GET",
+        headers: {'Content-Type': 'application/json'}
+    });
     let clientsArray = await getting.json();
-    for(let i = 0; i < clientsArray.length;i++){
-        if(clientsArray[i]._clientId.localeCompare("5de65d6c34b8d99f3f2aaf71") === 0){
+    for (let i = 0; i < clientsArray.length; i++) {
+        if (clientsArray[i]._clientId.localeCompare("5de65d6c34b8d99f3f2aaf71") === 0) {
 
             return 1;
         }
     }
     return 0;
 };
-async function getCoaches(){
+
+async function getCoaches() {
 
     let everyone = await fetch("/coaches/search?accountType=coach");
     let coachesArray = await everyone.json();
     await displayCoaches(coachesArray);
 }
 
-displayCoaches = async(coachesArray)=>{
+displayCoaches = async (coachesArray) => {
     cleanCards();
-    for(let i = 0; i<coachesArray.length; i++) {
+    for (let i = 0; i < coachesArray.length; i++) {
         let div = document.getElementById("grid");
 
         let card = document.createElement("div");
@@ -102,7 +107,7 @@ displayCoaches = async(coachesArray)=>{
             });
             row_div.appendChild(a);
             a.innerHTML = "HIRE ME!";
-        }else{
+        } else {
             let a = document.createElement('h1');
             a.innerHTML = "Already Hired";
             h2.appendChild(a);
@@ -110,56 +115,52 @@ displayCoaches = async(coachesArray)=>{
     }
 };
 
+// Used in client dashboard
+async function getExercises() {
+    try {
+        let headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        };
 
+        let userId = await fetch("/auth/getuserinfo");
+        userId = await userId.json();
 
+        let session = await fetch("/workouts/sessions/search?_clientId=" + userId.userAccountId + "&weekday=" + getWeekDay(), {
+            method: 'GET',
+            headers: headers,
+        });
 
+        let exercises = [];
+        if (session !== undefined) {
+            session = await session.json();
 
+            for (let i = 0; i < session.exercises.length; i++) {
+                let exercise = await fetch("/workouts/exercises/search?id=" + session.exercises[i], {
+                    method: 'GET',
+                    headers: headers
+                });
+                exercise = await exercise.json();
+                exercises.push(exercise);
+            }
+        } else {
+            exercises = ['-', '-', '-', '-', '-', '-'];
+        }
+        console.log(exercises);
 
+        dust.render("dashboard_partials\/schedule_table_row",
+            {exercises: exercises}, (err, out) =>
+                document.getElementById('scheduleTable').innerHTML = out);
+    } catch (err) {
+        console.log(err);
+    }
+}
 
+function getWeekDay() {
+    const weekdays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    return weekdays[new Date().getDay()];
+}
 
-
-
-//
-// async function getExercises() {
-//     try {
-//         let headers = {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json'
-//         };
-//
-//         let userId = await fetch("/auth/getuserinfo");
-//         userId = await userId.json();
-//
-//         let session = await fetch("/workouts/sessions/search?_clientId=" + userId.userAccountId + "&weekday=" + getWeekDay(), {
-//             method: 'GET',
-//             headers: headers,
-//         });
-//
-//         session = await session.json();
-//
-//         let exercises = [];
-//         for (let i = 0; i < session.exercises.length; i++) {
-//             let exercise = await fetch("/workouts/exercises/search?id=" + session.exercises[i], {
-//                 method: 'GET',
-//                 headers: headers
-//             });
-//             exercise = await exercise.json();
-//             exercises.push(exercise);
-//         }
-//
-//         dust.render("dashboard_partials\/schedule_table_row",
-//             {exercises: exercises}, (err, out) =>
-//                 document.getElementById('scheduleTable').innerHTML = out);
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
-//
-// function getWeekDay() {
-//     const weekdays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-//     return weekdays[new Date().getDay()];
-// }
-//
-// getExercises();
+getExercises();
 
 
