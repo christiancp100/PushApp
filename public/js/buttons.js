369851bdb070551603a57f86dd03e224f51d1c58
CarattiJ <jacopo.caratti@bluewin.ch>
2019-12-08 16:14:25 +0100

    buttons js to add eventlisteners

---
+++ public/js/buttons.js
@@ -0,0 +1,21 @@
+//myService
+function buttons(){
+    let buttons = document.querySelectorAll('.no-padding ul li a');
+    let buttonService = buttons[4];
+    buttonService.addEventListener("click", () => {
+        renderServices();
+    });
+    // let buttonDashboard = buttons[0];
+    // buttonDashboard.addEventListener("click", () => {
+    //     renderDashboard();
+    // });
+}
+
+// async function renderDashboard(){
+//     //container of the page
+//     let container = document.getElementsByClassName("container")[0];
+//     container.innerHTML = '';
+//         dust.render("dashboard_partials/schedule",{}, function(err, out) {
+//             container.innerHTML += out;
+//         });
+// }
