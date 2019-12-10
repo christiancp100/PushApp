4b019593ccc6f0f39e7d75223d0915306743d0a2
davidetrupia <trupid@usi.ch>
2019-12-10 14:57:56 +0100

    styled clients list of coach, added search feature

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -4,6 +4,7 @@ function buttons(){
 
     let buttonClients = buttons[1];
     buttonClients.addEventListener("click", () => {
+        renderStyle();//added
         renderClients();
     });
 
@@ -21,14 +22,24 @@ function buttons(){
 function clientButtons(){
     let buttons = document.querySelectorAll('.no-padding ul li a');
     let buttonCoach = buttons[4];
-    console.log(buttonCoach);
-    buttonCoach.addEventListener("click", () => {
-        renderCoaches();
+    buttonCoach.addEventListener("click", async() => {
+        await renderCoaches();
     });
 }
 
-async function renderClients(){
+
+renderStyle = async() =>{
+    resetDocument();
     let container = document.getElementsByClassName("container")[0];
+    dust.render('dashboard_partials/clients',{}, (err, out) => {
+        console.log(out);
+        container.innerHTML += out;
+    });
+};
+
+
+async function renderClients(){
+
     let hiringClients = await getHiringClients();
     let clients = [];
     for(let i = 0; i < hiringClients.length; i++){
@@ -36,14 +47,52 @@ async function renderClients(){
         let clientJson = await client.json();
         clients.push(clientJson[0]);
     }
-    resetDocument();
     if(clients.length !== 0) {
-        for (let i = 0; i < clients.length; i++) {
-            dust.render('dashboard_partials/client_card_for_list', {client: clients[i]}, (err, out) => {
-                container.innerHTML += out;
-            })
-        }
-    }else{
-
+            displayClients(clients);
     }
 }
+
+displayClients = (clients) =>{
+    let grid = document.getElementById("grid");
+    for (let i = 0; i < clients.length; i++) {
+        dust.render('dashboard_partials/client_card_for_list', {client: clients[i]}, (err, out) => {
+            grid.innerHTML += out;
+        })
+    }
+};
+cleanCards = () => {
+    let children = document.getElementById("grid").childNodes;
+    for (let i = 0; i < children.length; i++) {
+        children[i].remove();
+    }
+};
+
+toCamelCase = (text) => {
+    let ret = '';
+    for (let i = 0; i < text.length; i++) {
+        if (i === 0) {
+            ret += text.charAt(0).toUpperCase();
+        } else {
+            ret += text.charAt(i).toLowerCase();
+        }
+        if (text.charAt(i) === ' ') {
+            ret += text.charAt(i + 1).toUpperCase();
+            i++;
+        }
+    }
+    return ret;
+};
+
+searchClients = async()=>{
+    let nonformatted_txt = document.getElementById("last_name").value;
+    let txt = toCamelCase(nonformatted_txt);
+    if (txt === '' || txt === " ") {
+        cleanCards();
+        return await renderClients();
+    }
+    let found = await fetch("/clients/search?accountType=client&firstName=" + txt);
+    let foundArray = await found.json();
+    console.log(foundArray);
+    cleanCards();
+    await displayClients(foundArray);
+};
