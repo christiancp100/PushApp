e4158d061bae9711e92bfd34437a3ee8f143e9a6
erickgarro <erickgarro@gmail.com>
2019-12-13 23:49:23 +0100

    Fixed color inverted coach profile picture, added default profile images

--- models/UserAccount.js
+++ models/UserAccount.js
@@ -7,7 +7,7 @@ const UserAccountSchema = exports.UserAccountSchema = new Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     description: {type: String},
-    photo: { type: String},
+    photo: {type: String},
     birthday: {type: Date, required: true},
     sex: {type: String, required: true},
     email: {type: String, required: true},
